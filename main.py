072ba6004b14a74ea866255bf9b823c86e13b1c1
Nicolas Feuchster <nicolascfedu@students.highline.edu>
2021-04-10 02:24:59 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,154 @@
+from pip._vendor.msgpack.fallback import xrange
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+import qdarkstyle
+
+import uuid
+
+import sys
+import os
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setWindowTitle("Connect ME")
+
+        # Window setup
+        self.xpos = 1920//3
+        self.ypos = 1080//4
+        self.dimension = 30
+        self.aspectRatioWidth = 9
+        self.aspectRatioHeight = 16
+
+        self.windowWidth = self.dimension * self.aspectRatioWidth
+        self.windowHeight = self.dimension * self.aspectRatioHeight
+
+        # Show window
+        self.setGeometry(self.xpos, self.ypos, self.windowWidth, self.windowHeight)
+        # self.setWindowTitle("Connect ME")
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+
+        # # Label Application Name
+        # self.label = QtWidgets.QLabel(self)
+        # self.label.setText("Connect ME")
+        # self.label.move(self.windowWidth // 2.2, self.windowHeight // 32)
+
+        # Set StyleSheet to Fusion by Default
+        self.setStyleSheet('Fusion')
+
+        # Delete File
+        self.deleteFileButton = QPushButton(self)
+        self.deleteFileButton.setText("Murder File(s)")
+        self.deleteFileButton.clicked.connect(self.removeSel)
+
+        # List of Files
+        self.listWidget = QtWidgets.QListWidget()
+        self.listWidget.setGeometry(QtCore.QRect(120, 40, 256, 192))
+        self.listWidget.setObjectName("listView")
+        self.listWidget.move(50, 500)
+
+        # Host Label Text-Box
+        self.hostLabelTextBox = QtWidgets.QLabel(self)
+        self.uniqueID = "Your ID: \n" + str(uuid.uuid4())  # User's unique ID
+        self.hostLabelTextBox.setText(self.uniqueID)
+
+        # Host Field Text-Box
+        self.hostFieldTextBox = QLineEdit(self)
+        self.hostFieldTextBox.setPlaceholderText("Reciever's Unique ID")
+
+
+        # Pick Files Button
+        self.pickFilesButton = QPushButton(self)
+        self.pickFilesButton.setText("Pick Files")
+        self.pickFilesButton.move(self.windowWidth // 2, self.windowHeight // 4)
+        self.pickFilesButton.clicked.connect(self.pickFiles)
+
+        # Send Files Button
+        # Will save the unique ID of the receiver when clicked as well
+        self.sendFilesButton = QPushButton(self)
+        self.sendFilesButton.setText("Send Files")
+        self.sendFilesButton.move(self.windowWidth // 4, self.windowHeight // 4)
+        self.sendFilesButton.clicked.connect(self.sendFiles)
+
+        # Dark Theme/Light Theme Button
+        self.darkLightButton = QPushButton(self)
+        self.darkLightButton.setText("Light") # Light is set as default stylesheet mode
+        self.darkLightButton.clicked.connect(self.darkLight)
+        # Used to switch between dark and light mode
+        self._darkLight_flag = True
+
+        # Add Widgets to Layout
+        layout.addWidget(self.deleteFileButton)
+        layout.addWidget(self.listWidget)
+        layout.addWidget(self.hostLabelTextBox)
+        layout.addWidget(self.hostFieldTextBox)
+        layout.addWidget(self.sendFilesButton)
+        layout.addWidget(self.pickFilesButton)
+        layout.addWidget(self.darkLightButton)
+
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        # Set the central widget of the Window. Widget will expand
+        # to take up all the space in the window by default.
+        self.setCentralWidget(widget)
+
+    def removeSel(self):
+        listItems = self.listWidget.selectedItems()
+        if not listItems: return
+        for item in listItems:
+            self.listWidget.takeItem(self.listWidget.row(item))
+
+    def pickFiles(self):
+        # Mac OS
+        fname = QFileDialog.getOpenFileNames(self, "Open File", "/Users/admin")
+        self.listWidget.addItems(fname[0])
+
+    def sendFiles(self):
+        print("Send These Files")
+        items = []
+
+        for index in xrange(self.listWidget.count()):
+            items.append(self.listWidget.item(index))
+
+        # Saves all the items as strings in the list in an array
+        labels = [i.text() for i in items]
+
+        print(labels)
+
+        # Saves receiver's unique ID to a variable
+        self.hostFieldValue = self.hostFieldTextBox.text()
+        print(self.hostFieldValue)
+
+
+    def darkLight(self):
+
+        if(self._darkLight_flag == True):
+            self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+            self.darkLightButton.setText("Dark")
+            self._darkLight_flag = False
+
+        elif (self._darkLight_flag == False):
+            self.setStyleSheet('Fusion')
+            self.darkLightButton.setText("Light")
+            self._darkLight_flag = True
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = MyWindow()
+    win.show()
+    sys.exit(app.exec_())
+
+window()
+
+
+
